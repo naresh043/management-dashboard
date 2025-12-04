4781871920e6665b3a3e6ba63e9ee37bb4a654be
NareshSanjeev <naresh732003@gmail.com>
2025-12-04 16:43:36 +0530

    Update API_BASE_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:10000" //"https://management-backend-yihw.onrender.com"; 
+const API_BASE_URL ="https://management-backend-yihw.onrender.com"; 
 // "http://localhost:10000"
 
 export default API_BASE_URL;
